2c0ce845653a5f0a2916982c350a4a6a6f24c98a
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-10-24 18:43:53 +0300

    Async Programming

---
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncAwait.Task2.CodeReviewChallenge.Headers;
+using CloudServices.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace AsyncAwait.Task2.CodeReviewChallenge.Middleware;
+
+public class StatisticMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private readonly IStatisticService _statisticService;
+
+    public StatisticMiddleware(RequestDelegate next, IStatisticService statisticService)
+    {
+        _next = next;
+        _statisticService = statisticService ?? throw new ArgumentNullException(nameof(statisticService));
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        string path = context.Request.Path;
+
+        await _statisticService.RegisterVisitAsync(path);
+        await UpdateHeaders(context, path);
+
+        await _next(context);
+    }
+
+    private async Task UpdateHeaders(HttpContext context, string path)
+    {
+        var visits = await _statisticService.GetVisitsCountAsync(path);
+
+        context.Response.Headers.Add(
+            CustomHttpHeaders.TotalPageVisits,
+            visits.ToString());
+    }
+}
