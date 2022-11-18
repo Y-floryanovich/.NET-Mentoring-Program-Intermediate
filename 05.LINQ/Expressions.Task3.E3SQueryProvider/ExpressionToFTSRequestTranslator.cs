f1a71746705e20c2e1c0e36c04add0af442f55ae
Yana Flaryanovich <Yana_Flaryanovich@epam.com>
2022-11-18 10:13:00 +0300

    LINQ,  IQueryable

---
+++ 05.LINQ/Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Expressions.Task3.E3SQueryProvider
+{
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        readonly StringBuilder _resultStringBuilder;
+
+        public ExpressionToFtsRequestTranslator()
+        {
+            _resultStringBuilder = new StringBuilder();
+        }
+
+        public string Translate(Expression exp)
+        {
+            Visit(exp);
+
+            return _resultStringBuilder.ToString();
+        }
+
+        #region protected methods
+
+        private Expression GetExpression(string left, string right, MethodCallExpression node)
+        {
+            Visit(node.Object);
+            var arg = node.Arguments[0];
+            if (arg.NodeType == ExpressionType.Constant)
+            {
+                _resultStringBuilder.Append(left);
+                Visit(arg);
+                _resultStringBuilder.Append(right);
+            }
+            else
+                Visit(arg);
+            return node;
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable)
+                && node.Method.Name == "Where")
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
+
+                return node;
+            }
+
+            if (node.Method.DeclaringType == typeof(string))
+            {
+                switch (node.Method.Name)
+                {
+                    case "Equals":
+                        return GetExpression("(", ")", node);
+
+                    case "Contains":
+                        return GetExpression("(*", "*)", node);
+
+                    case "StartsWith":
+                        return GetExpression("(", "*)", node);
+
+                    case "EndsWith":
+                        return GetExpression("(*", ")", node);
+                }
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+                    var memberAccess = node.Left.NodeType == ExpressionType.MemberAccess ? node.Left : (node.Right.NodeType == ExpressionType.MemberAccess ? node.Right : null);
+                    var constant = node.Left.NodeType == ExpressionType.Constant ? node.Left : node.Right.NodeType == ExpressionType.Constant ? node.Right : null;
+
+                    if (memberAccess is null || constant is null)
+                        throw new NotSupportedException($"Expression '{node}' is not supported");
+
+                    Visit(memberAccess);
+                    _resultStringBuilder.Append("(");
+                    Visit(constant);
+                    _resultStringBuilder.Append(")");
+                    break;
+
+                case ExpressionType.AndAlso:
+                    Visit(node.Left);
+                    _resultStringBuilder.Append(" AND ");
+                    Visit(node.Right);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
+            };
+
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultStringBuilder.Append(node.Member.Name).Append(":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultStringBuilder.Append(node.Value);
+
+            return node;
+        }
+
+        #endregion
+    }
+}
